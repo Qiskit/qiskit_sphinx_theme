a3c2e097e9f95fa1f0e14607374cc0ea670eaaa5
Paul Nation <nonhermitian@gmail.com>
2020-11-06 15:34:14 -0500

    return explicit parallel safe

--- qiskit_sphinx_theme/__init__.py
+++ qiskit_sphinx_theme/__init__.py
@@ -17,3 +17,6 @@ def get_html_theme_path():
 # See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
 def setup(app):
     app.add_html_theme('qiskit_sphinx_theme', path.abspath(path.dirname(__file__)))
+
+    # return explicit parallel safe
+    return {'parallel_read_safe': True, 'parallel_write_safe': True}
