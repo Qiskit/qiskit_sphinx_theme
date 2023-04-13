13e1ce47a0950df923bcbc25af6784814cf9660d
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-04-13 14:02:10 -0600

    Use autodoc in example_docs (#265)
    Part of https://github.com/Qiskit/qiskit_sphinx_theme/issues/220.
    
    This makes our class page more realistic:
    
    ![Screenshot 2023-04-12 at 7 50 23
    AM](https://user-images.githubusercontent.com/14852634/231478634-9fcf0405-0b8c-46fb-9918-f9d42242df75.png)
    
    We also now have a module page:
    
    ![Screenshot 2023-04-12 at 7 50 48
    AM](https://user-images.githubusercontent.com/14852634/231478744-9ab794a3-6a24-4a0c-844c-05edc5a6d037.png)
    
    And a method page:
    
    ![Screenshot 2023-04-12 at 7 51 07
    AM](https://user-images.githubusercontent.com/14852634/231478831-2818d573-fa11-4cb3-9755-f5384acddfbc.png)

---
+++ example_docs/api_example/electron.py
@@ -0,0 +1,32 @@
+
+class Electron:
+    """A representation of an electron."""
+
+    def __init__(self, size: str = None, name: str = None) -> None:
+        """Create an electron.
+
+        :param size: How big should this thing be?
+        :param name: The name we'll call the electron. Nicknames preferred.
+        :raises ValueError: You did something wrong
+        """
+
+    @property
+    def charge(self):
+        pass
+
+    @property
+    def mass(self) -> float:
+        """The mass of the electron."""
+        return 2.1
+
+    @property
+    def really_long_named_attribute_that_probably_does_not_fit_nicely(self):
+        """A bit too verbose if you ask me."""
+
+    def compute_momentum(self, velocity: float) -> float:
+        """Compute the electron's velocity.
+
+        :param velocity: The electron's velocity
+        :return: The computed momentum.
+        :raises ValueError: You did something wrong
+        """
