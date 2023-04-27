4487dccaab8d9a861ef653b8db2425ac18529f31
Eric Arellano <ericarellano@me.com>
2023-04-27 09:48:20 -0400

    Reorder CI

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,5 +1,5 @@
 const { expect, test } = require("@jest/globals");
 
 test("dummy test to check Jest infrastructure works. Remove after", () => {
-    expect(true).toBe(true)
-})
+  expect(true).toBe(true);
+});
