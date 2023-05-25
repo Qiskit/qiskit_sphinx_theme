0e0cbd2cab4e56f128f729fff3089727f68e860b
Eric Arellano <ericarellano@me.com>
2023-05-25 13:55:49 -0600

    Try lowering the threshold

--- tests/js/jest.setup.js
+++ tests/js/jest.setup.js
@@ -1,3 +1,22 @@
 const { toMatchImageSnapshot } = require("jest-image-snapshot");
 
-expect.extend({ toMatchImageSnapshot });
+// The below adds `toMatchImageSnapshot` to Jest's `expect` so that you can call
+// `expect(screenshot).toMatchImageSnapshot()`.
+//
+// We use JavaScript's partial application mechanism to pre-set the config used so that you don't
+// have to keep passing the same config with each call to toMatchImageSnapshot().
+//
+// This config reduces the sensitivity of our tests because CI and local may have slight
+// differences, such as how fonts render. We use SSIM (structural similarity index), rather than
+// pixel-by-pixel comparisons, because it has fewer false positives. See
+// https://github.com/americanexpress/jest-image-snapshot#recommendations-when-using-ssim-comparison.
+const snapshotConfig = {
+  comparisonMethod: "ssim",
+  failureThreshold: 0.01,
+  failureThresholdType: "percent",
+};
+expect.extend({
+  toMatchImageSnapshot(received) {
+    return toMatchImageSnapshot.call(this, received, snapshotConfig);
+  },
+});
