515289200bf8b0803cfd9401d113c20c06a3ab58
Eric Arellano <ericarellano@me.com>
2023-06-05 11:28:41 -0600

    Add encodings

--- tests/py/web_components_test.py
+++ tests/py/web_components_test.py
@@ -4,8 +4,10 @@ from pathlib import Path
 
 
 def test_top_nav_bar():
-    furo = Path("qiskit_sphinx_theme/furo/base/static/js/web-components/top-nav-bar.js").read_text()
+    furo = Path("qiskit_sphinx_theme/furo/base/static/js/web-components/top-nav-bar.js").read_text(
+        "utf-8"
+    )
     pytorch = Path(
         "qiskit_sphinx_theme/pytorch_base/static/js/web-components/top-nav-bar.js"
-    ).read_text()
+    ).read_text("utf-8")
     assert furo == pytorch
