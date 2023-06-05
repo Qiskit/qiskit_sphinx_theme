86858134190ea375cb876c41e533c78d61e68d39
Alberto Maldonado <Alberto.Maldonado.Romo@ibm.com>
2023-06-05 10:21:50 -0400

    Add Black and isort autoformatters (#356)
    Utilize both Black and Isort to apply formatting changes across all
    Python files in order to modify the existing format used in
    qiskit_sphinx_theme.

--- docs_guide/conf.py
+++ docs_guide/conf.py
@@ -8,25 +8,26 @@
 
 import datetime
 
-project = 'Qiskit Docs Guide'
-copyright = f'2022-{datetime.date.today().year}, Qiskit Development Team'
-author = 'Qiskit Development Team'
-release = '1.0'
+project = "Qiskit Docs Guide"
+copyright = f"2022-{datetime.date.today().year}, Qiskit Development Team"
+author = "Qiskit Development Team"
+release = "1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['jupyter_sphinx', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx_toolbox.confval']
+extensions = [
+    "jupyter_sphinx",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx_toolbox.confval",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
 
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'qiskit_sphinx_theme'
+html_theme = "qiskit_sphinx_theme"
 
 # -- Intersphinx configuration ------------------------------------------------
 
