220a3d36357c75f8a97b32b08d6ce261c36a5e2a
Eric Arellano <ericarellano@me.com>
2023-06-06 12:02:53 -0600

    Better grammar in test description

--- tests/js/snapshots.test.js
+++ tests/js/snapshots.test.js
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test.describe("footer", () => {
-  test("to include page analytics", async ({ page }) => {
+  test("includes page analytics", async ({ page }) => {
     await page.goto("");
     const footer = page.locator("footer");
     await expect(footer).toHaveScreenshot();
