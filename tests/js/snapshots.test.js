8d19c470d2b9d0e209fed7c19b07dcc20b0cfbd1
Eric Arellano <ericarellano@me.com>
2023-06-06 11:51:57 -0600

    Finish footer tests

--- tests/js/snapshots.test.js
+++ tests/js/snapshots.test.js
@@ -6,4 +6,22 @@ test.describe("footer", () => {
     const footer = page.locator("footer");
     await expect(footer).toHaveScreenshot();
   });
+
+  test("shows analytics options in purple when hovered", async ({page}) => {
+    await page.goto("");
+    const yesOption = page.locator("a.helpful-question.yes-link");
+    await yesOption.hover();
+
+    const analytics = page.locator("div.helpful-container");
+    await expect(analytics).toHaveScreenshot();
+  });
+
+  test("says 'thank you' when analytics clicked", async ({page}) => {
+    await page.goto("");
+    const yesOption = page.locator("a.helpful-question.yes-link");
+    await yesOption.click();
+
+    const analytics = page.locator("div.helpful-container");
+    await expect(analytics).toHaveScreenshot();
+  });
 });
