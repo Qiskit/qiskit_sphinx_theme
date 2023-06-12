f55427e88ee471d2e2534a41f51e4fbe68456173
Eric Arellano <ericarellano@me.com>
2023-06-12 09:52:56 -0600

    Add snapshot test

--- playwright.config.js
+++ playwright.config.js
@@ -5,6 +5,11 @@ const baseURL = "http://127.0.0.1:8080";
 export default defineConfig({
   outputDir: "snapshot_results",
   workers: process.env.CI ? 1 : undefined,
+  expect: {
+    toHaveScreenshot: {
+      threshold: 0.0, // We expect colors to be exact matches.
+    },
+  },
   use: {
     baseURL,
   },
--- tests/js/snapshots.test.js
+++ tests/js/snapshots.test.js
@@ -1,5 +1,11 @@
 import { expect, test } from "@playwright/test";
 
+test("left side bar only uses purple for current page", async ({ page }) => {
+  await page.goto("");
+  const leftToC = page.locator("div.sidebar-tree");
+  await expect(leftToC).toHaveScreenshot();
+});
+
 test.describe("footer", () => {
   test("includes page analytics", async ({ page }) => {
     await page.goto("");
