5a0be62aa2aaa294ea23ddd0add8f74e037261dd
Eric Arellano <ericarellano@me.com>
2023-06-12 11:33:46 -0600

    Fix Furo tutorials having purple border

--- tests/js/snapshots.test.js
+++ tests/js/snapshots.test.js
@@ -7,3 +7,9 @@ test.describe("footer", () => {
     await expect(footer).toHaveScreenshot();
   });
 });
+
+test("tutorials do not have purple border", async ({ page }) => {
+  await page.goto("sphinx_guide/notebook_gallery.html");
+  const tutorial = page.locator("div.nbsphinx-gallery");
+  await expect(tutorial).toHaveScreenshot();
+});
