a8676fe2b60cc499fcc28d0a6230b084c02a8b0c
Eric Arellano <ericarellano@me.com>
2023-06-12 10:14:32 -0600

    Remove footer hover test

--- tests/js/snapshots.test.js
+++ tests/js/snapshots.test.js
@@ -148,15 +148,6 @@ test.describe("footer", () => {
     await expect(footer).toHaveScreenshot();
   });
 
-  test("shows analytics options in purple when hovered", async ({ page }) => {
-    await page.goto("");
-    const yesOption = page.locator("a.helpful-question.yes-link");
-    await yesOption.hover();
-
-    const analytics = page.locator("div.helpful-container");
-    await expect(analytics).toHaveScreenshot();
-  });
-
   test("says 'thank you' when analytics clicked", async ({ page }) => {
     await page.goto("");
     const yesOption = page.locator("a.helpful-question.yes-link");
