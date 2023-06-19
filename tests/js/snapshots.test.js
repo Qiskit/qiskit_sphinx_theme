7116bb432fba041aaf1f1222a2f7b16effa09fad
Eric Arellano <ericarellano@me.com>
2023-06-19 08:57:39 -0600

    Remove drop shadows from Furo

--- tests/js/snapshots.test.js
+++ tests/js/snapshots.test.js
@@ -218,3 +218,10 @@ test("admonitions use Carbon style", async ({ page }) => {
   const admonitions = page.locator("section#admonitions");
   await expect(admonitions).toHaveScreenshot();
 });
+
+test("Sphinx Design elements have no shadows", async ({ page }) => {
+  await page.goto("sphinx_guide/panels.html");
+  await page.locator(".sd-dropdown").first().click();
+  const pageContents = page.locator("section#panels");
+  await expect(pageContents).toHaveScreenshot();
+});
