cbc4920d918d1b42c5f1590c5ff79ca883846f9c
Eric Arellano <ericarellano@me.com>
2023-06-23 09:32:05 -0600

    Rename qiskit-card-container to qiskit-card

--- qiskit_sphinx_theme/directives.py
+++ qiskit_sphinx_theme/directives.py
@@ -50,7 +50,7 @@ class QiskitCardDirective(Directive):
         card_rst = f"""
 .. raw:: html
 
-    <div class="qiskit-card-container card card-body" link={link}>
+    <div class="qiskit-card card card-body" link={link}>
         <div class="qiskit-card-title-container"><h4>{header}</h4></div>
         <p class="qiskit-card-summary">{card_description}</p>
         <div class="qiskit-card-image-container"><img src='{image_source}'></div>
