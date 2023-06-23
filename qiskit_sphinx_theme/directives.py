80d204c301e3d21e2c16d8451a5f64e3048cb05f
Eric Arellano <ericarellano@me.com>
2023-06-23 09:41:48 -0600

    Get rid of class for p element
    To avoid specificity wars, we now use div in front of the .qiskit-card class always

--- qiskit_sphinx_theme/directives.py
+++ qiskit_sphinx_theme/directives.py
@@ -52,7 +52,7 @@ class QiskitCardDirective(Directive):
 
     <div class="qiskit-card card card-body" link={link}>
         <div class="qiskit-card-title-container"><h4>{header}</h4></div>
-        <p class="qiskit-card-summary">{card_description}</p>
+        <p>{card_description}</p>
         <div class="qiskit-card-image-container"><img src='{image_source}'></div>
     </div>
 """
