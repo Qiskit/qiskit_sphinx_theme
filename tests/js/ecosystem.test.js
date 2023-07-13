3cbf2dcce421fe9a08ee284a47746a67c4090206
Eric Arellano <ericarellano@me.com>
2023-07-13 09:59:30 -0600

    Add tests for Ecosystem removing top nav bar

--- tests/js/ecosystem.test.js
+++ tests/js/ecosystem.test.js
@@ -13,7 +13,75 @@
 
 import { expect, test } from "@playwright/test";
 
-import { setMobile } from "./utils";
+import { setMobile, setDesktop, click, scrollDown, getPosition } from "./utils";
+
+test.describe("Reverted Qiskit top nav bar does not break positioning of", () => {
+  test("Furo's menu bars when scrolled down", async ({ page }) => {
+    await page.goto("sphinx_guide/lists.html");
+
+    const check = async () => {
+      const pageToCPosition = await getPosition(
+        page,
+        "div.toc-title-container",
+      );
+      expect(pageToCPosition).toEqual({ x: 1344, y: 0 });
+
+      const searchPosition = await getPosition(page, "input.sidebar-search");
+      expect(searchPosition).toEqual({ x: 0, y: 8 });
+
+      await setMobile(page);
+      const mobileHeaderPosition = await getPosition(
+        page,
+        "header.mobile-header",
+      );
+      expect(mobileHeaderPosition).toEqual({ x: 0, y: 0 });
+    };
+
+    // First check after scrolling down a little bit.
+    await scrollDown(page, 200);
+    await check();
+
+    // Then scroll to the bottom of the page. We use the home page because it's short.
+    await setDesktop(page);
+    await page.goto("");
+    await scrollDown(page, 1000);
+    await check();
+  });
+
+  test("the top of # anchor links", async ({ page }) => {
+    const checkHeader = async (x, y) => {
+      await page.goto("sphinx_guide/lists.html#definition-lists");
+      const headerPosition = await getPosition(
+        page,
+        "section#definition-lists > h2",
+      );
+      expect(headerPosition).toEqual({ x, y });
+    };
+
+    await checkHeader(328, 0);
+
+    await setMobile(page);
+    // Go to a new page first to reset the scroll state.
+    await page.goto("");
+    await checkHeader(8, 56);
+  });
+
+  test("the side menus when they're expanded on mobile", async ({ page }) => {
+    await setMobile(page);
+    await page.goto("");
+
+    await click(page, "div.header-right label.toc-overlay-icon i");
+    const pageToCPosition = await getPosition(page, "div.toc-title-container");
+    expect(pageToCPosition).toEqual({ x: 135, y: 0 });
+
+    // Reload the page to close the nav bar.
+    await page.goto("");
+
+    await click(page, "div.header-left i");
+    const searchPosition = await getPosition(page, "input.sidebar-search");
+    expect(searchPosition).toEqual({ x: 0, y: 8 });
+  });
+});
 
 test("mobile header uses Furo design", async ({ page }) => {
   await setMobile(page);
