72bd0fa68debe002de6fa477433d7809292fe931
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-07-17 14:41:03 -0600

    Ecosystem: switch default color scheme to Magenta 70 but allow changing (#493)
    Closes https://github.com/Qiskit/qiskit_sphinx_theme/issues/487.
    
    It's important that we distinguish between core Qiskit and the
    Ecosystem. The new color scheme allows blue, pink, and purple. Projects
    more aligned with IBM, like qiskit-ibm-runtime, should use blue.
    Projects more separated from IBM should use more pink. And projects
    in-between could maybe stick with purple.
    
    So, we use magenta for the default ecosystem color. 
    
    But, we define `--qiskit-color-{purple,magenta,blue}` so that themes can
    override the color. They can do this in `conf.py` using this code (the
    feature comes from Furo):
    
    ```python
        html_theme_options = {
            "light_css_variables": {
                "color-brand-primary": "var(--qiskit-color-blue)",
            }
        }
    ```
    
    ![Screenshot 2023-07-17 at 1 44 15
    PM](https://github.com/Qiskit/qiskit_sphinx_theme/assets/14852634/f31cfae9-152d-429d-a9bc-2958e4786b00)
    
    All colors come from Carbon's color palette and use the shade 70 because
    it looks more professional ("utility") and coincides with using Purple
    70 for Qiskit.

--- tests/js/ecosystem.test.js
+++ tests/js/ecosystem.test.js
@@ -13,11 +13,36 @@
 
 import { expect, test } from "@playwright/test";
 
-import { setMobile } from "./utils";
+import { click, setMobile } from "./utils";
+
+test.describe("left side bar", () => {
+  test("renders correctly", async ({ page }) => {
+    await page.goto("");
+    const leftToC = page.locator(".sidebar-drawer");
+    await expect(leftToC).toHaveScreenshot();
+  });
+
+  test("translations use ecosystem colors", async ({ page }) => {
+    await page.goto("");
+    await click(page, "div.qiskit-translations-container i");
+    const translations = page.locator("div.qiskit-translations-container");
+    await expect(translations).toHaveScreenshot();
+  });
+});
 
 test("mobile header uses Furo design", async ({ page }) => {
   await setMobile(page);
   await page.goto("");
   const header = page.locator("header.mobile-header");
   await expect(header).toHaveScreenshot();
+});
+
+test("footer uses ecosystem colors", async ({ page }) => {
+  await page.goto("");
+
+  const yesOption = page.locator("div.qiskit-analytics-container a").first();
+  await yesOption.hover();
+
+  const footer = page.locator("footer");
+  await expect(footer).toHaveScreenshot();
 });
