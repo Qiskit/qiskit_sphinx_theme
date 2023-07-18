718d15913119bcf790201c8373bd5b30b2c56a60
Eric Arellano <ericarellano@me.com>
2023-07-18 10:28:44 -0600

    Clarify where values come from

--- tests/js/ecosystem.test.js
+++ tests/js/ecosystem.test.js
@@ -18,7 +18,7 @@ import { click, getPosition, setMobile, setDesktop, scrollDown } from "./utils";
 // These tests use `getPosition` to get the exact x and y coordinates of HTML elements.
 // It's possible we'll make design changes in the future that change those coordinates;
 // if so, first check the example docs to make sure they look right, and then update
-// the expected coordinates below.
+// the expected coordinates below by using the values from the failed test.
 test.describe("Reverted Qiskit top nav bar does not break positioning of", () => {
   test("Furo's menu bars when scrolled down", async ({ page }) => {
     await page.goto("sphinx_guide/lists.html");
